55201edc078907a56ddf26b28f579a59953c62c2
Vinicius <vinijaguare@gmail.com>
2024-07-03 11:12:29 -0300

    create constraint file

---
+++ app/src/main/java/com/emodiario/Constraints.kt
@@ -0,0 +1,8 @@
+package com.emodiario
+
+class Constraints {
+    companion object {
+        const val BASE_URL = "https://058a-2804-14d-ae84-8456-487c-46f-ce5-770b.ngrok-free.app"
+        const val PHONE_NUMBER_LENGTH = 11
+    }
+}
