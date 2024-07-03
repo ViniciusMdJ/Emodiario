a1ad640d9f9ddd0229edb1b622db51996b9c5172
Vinicius <vinijaguare@gmail.com>
2024-07-03 11:41:11 -0300

    update main activity

--- app/src/main/java/com/emodiario/MainActivity.kt
+++ app/src/main/java/com/emodiario/MainActivity.kt
@@ -3,7 +3,7 @@ package com.emodiario
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import com.emodiario.domain.preferences.Prefs
 import com.emodiario.navigation.MainNavHost
 import com.emodiario.ui.theme.EmodiarioTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -12,11 +12,11 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        enableEdgeToEdge()
         setContent {
             EmodiarioTheme {
                 MainNavHost(
                     onBackPressed = { this@MainActivity.onBackPressedDispatcher.onBackPressed() },
+                    prefs = Prefs(this)
                 )
             }
         }
