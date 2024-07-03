f695730c0256eacf0c56b2900ccfd698dfbb55f4
Vinicius <vinijaguare@gmail.com>
2024-07-03 11:31:55 -0300

    create profile screen

---
+++ app/src/main/java/com/emodiario/presentation/profile/Profile.kt
@@ -0,0 +1,223 @@
+package com.emodiario.presentation.profile
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.emodiario.R
+import com.emodiario.domain.model.User
+import com.emodiario.presentation.common.ui_components.NetworkImage
+import com.emodiario.ui.theme.EmodiarioTheme
+
+@Composable
+fun ProfileScreen(
+    onBackPressed: () -> Unit,
+    onLogoutPressed: () -> Unit,
+    viewModel: ProfileViewModel = hiltViewModel()
+) {
+
+    val name = viewModel.uiState.name.collectAsState()
+    val photoUrl = viewModel.uiState.photoUrl.collectAsState()
+    val showDialog = viewModel.uiState.showDialog.collectAsState()
+
+    ScreenContent(
+        name = name.value,
+        photoUrl = photoUrl.value,
+        showDialog = showDialog.value,
+        toggleShowDialog = viewModel.uiState::updateShowDialog,
+        onBackPressed = onBackPressed,
+        onLogoutPressed = onLogoutPressed
+    )
+}
+
+@Composable
+fun ScreenContent(
+    name: String,
+    photoUrl: String,
+    showDialog: Boolean,
+    toggleShowDialog: (Boolean) -> Unit,
+    onBackPressed: () -> Unit,
+    onLogoutPressed: () -> Unit
+) {
+    Scaffold(
+        topBar = {
+            TopAppBarProfile(
+                imageUrl = photoUrl,
+                userName = name,
+                iconNavigation = {
+                    Icon(
+                        modifier = Modifier.clickable { onBackPressed() },
+                        painter = painterResource(id = R.drawable.ic_arrow_back),
+                        contentDescription = "Back buttom",
+                        tint = Color.White
+                    )
+                }
+            )
+        }
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(it)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Bottom
+        ) {
+            Surface(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                color = MaterialTheme.colorScheme.surface
+            ) {
+                Row(
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .clickable { toggleShowDialog(true) },
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_logout),
+                        contentDescription = "Logout",
+                        tint = colorResource(id = R.color.error)
+                    )
+                    Text(
+                        text = stringResource(id = R.string.logout),
+                        style = MaterialTheme.typography.titleLarge,
+                        color = colorResource(id = R.color.error),
+                    )
+                }
+            }
+        }
+        if (showDialog) {
+            AlertDialog(
+                title = { Text(stringResource(id = R.string.title_confirmation_logout)) },
+                onDismissRequest = { },
+                confirmButton = {
+                    TextButton(
+                        onClick = {
+                            onLogoutPressed()
+                        }
+                    ) {
+                        Text(stringResource(id = R.string.yes))
+                    }
+                },
+                dismissButton = {
+                    TextButton(
+                        onClick = { toggleShowDialog(false) }
+                    ) {
+                        Text(stringResource(id = R.string.no))
+                    }
+                },
+            )
+
+        }
+
+    }
+}
+
+@Composable
+fun TopAppBarProfile(
+    imageUrl: String?,
+    userName: String,
+    iconNavigation: @Composable () -> Unit
+) {
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = listOf(
+                        MaterialTheme.colorScheme.secondary,
+                        MaterialTheme.colorScheme.primary
+                    )
+                )
+            )
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Row(
+            modifier = Modifier.align(Alignment.Start),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.Start)
+        ) {
+            iconNavigation()
+            Text(
+                text = stringResource(id = R.string.profile_title),
+                color = Color.White,
+                style = MaterialTheme.typography.titleLarge,
+            )
+        }
+        NetworkImage(
+            imageUrl = imageUrl,
+            modifier = Modifier
+                .size(80.dp)
+                .clip(RoundedCornerShape(50.dp))
+        )
+        Text(
+            text = userName,
+            color = Color.White,
+            style = MaterialTheme.typography.titleLarge
+        )
+    }
+
+}
+
+@Preview
+@Composable
+fun ProfileScreenPreview() {
+    EmodiarioTheme {
+        ScreenContent(
+            name = "Vinicus",
+            photoUrl = "",
+            showDialog = true,
+            toggleShowDialog = {},
+            onLogoutPressed = {},
+            onBackPressed = {}
+        )
+    }
+}
+
+@Preview
+@Composable
+fun TopAppBarProfilePreview() {
+    EmodiarioTheme {
+        TopAppBarProfile(
+            imageUrl = null,
+            userName = "Vinicius",
+            iconNavigation = {
+                Icon(
+                    modifier = Modifier.clickable { },
+                    painter = painterResource(id = R.drawable.ic_arrow_back),
+                    contentDescription = "Back buttom",
+                    tint = Color.White
+                )
+            }
+        )
+    }
+}
---
+++ app/src/main/java/com/emodiario/presentation/profile/ProfileViewModel.kt
@@ -0,0 +1,38 @@
+package com.emodiario.presentation.profile
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.emodiario.domain.model.User
+import com.emodiario.domain.use_cases.GetProfileUseCase
+import com.emodiario.presentation.common.toMessageError
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import javax.inject.Inject
+
+@HiltViewModel
+class ProfileViewModel @Inject constructor(
+    private val profileRepository: GetProfileUseCase
+) : ViewModel() {
+    val uiState = ProfileUiState()
+
+    fun setContent() {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                uiState.setLoading()
+                val profile = profileRepository(1)
+                uiState.updateName(profile.name)
+                uiState.updatePhotoUrl(profile.photoUrl ?: "")
+                Log.i("ProfileViewModel", "Profile: $profile")
+            } catch (e: HttpException) {
+                Log.e("ProfileViewModel", e.toMessageError(), e)
+                uiState.setError(e.toMessageError())
+            } catch (e: Exception) {
+                Log.e("ProfileViewModel", "Error: ${e.message}", e)
+                uiState.setError(message = e.message.orEmpty())
+            }
+        }
+    }
+}
