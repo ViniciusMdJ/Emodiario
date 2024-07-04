f7eb86471b73601820c6d6fd10806546aa2d3ffc
Vinicius <vinijaguare@gmail.com>
2024-07-04 00:19:58 -0300

    update url

--- app/src/main/java/com/emodiario/Constraints.kt
+++ app/src/main/java/com/emodiario/Constraints.kt
@@ -2,7 +2,7 @@ package com.emodiario
 
 class Constraints {
     companion object {
-        const val BASE_URL = "https://058a-2804-14d-ae84-8456-487c-46f-ce5-770b.ngrok-free.app"
+        const val BASE_URL = "https://light-bengal-concrete.ngrok-free.app"
         const val PHONE_NUMBER_LENGTH = 11
     }
 }
